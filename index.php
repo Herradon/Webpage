1592d0f0227cf11228cd4cdb3e91b847f5efc4d7
Herradon <73962361+Herradon@users.noreply.github.com>
2026-09-07 17:52:41 +0200

    diseño y maquetación

--- index.php
+++ index.php
@@ -65,13 +65,6 @@ session_start();
             información y asistir en diferentes procesos del negocio
             durante las 24 horas del día.
 
-            <br><br>
-
-            Creamos agentes adaptados a las necesidades de cada
-            negocio para aplicarlos en una página web, aportando
-            soluciones integradas con WhatsApp, correo electrónico
-            y otros servicios.
-
         </p>
 
     </div>
@@ -80,23 +73,22 @@ session_start();
 
             <div>
 
-                 <img id="assistantAvatar" src="" alt="Asistente de Diseño y Desarrollo Web">
+                 <img src="img/asesor.png" alt="Asistente de Diseño y Desarrollo Web">
                 <h1>Asesoramos</h1>
                 <p>Identificamos las oportunidades ocultas de tu negocio mediante la Inteligencia Artificial. Evaluamos tu infraestructura actual para diseñar una estrategia clara de automatización inteligente. Te guiamos en la selección de tecnologías y en la definición de roles para agentes de IA autónomos, asegurando que cada solución esté perfectamente alineada con tus objetivos comerciales y normativas éticas.</p>
-                
 
             </div>
 
             <div>
 
-                <img id="assistantAvatar" src="img/desarrollo.png" alt="Asistente de Diseño y Desarrollo Web">
+                <img  src="img/desarrollo.png" alt="Asistente de Diseño y Desarrollo Web">
                 <h1>Desarrollamos</h1>
                 <p>Construimos el cerebro digital que tu empresa necesita. Diseñamos modelos personalizados y programamos agentes de IA avanzados capaces de razonar, interactuar y tomar decisiones contextuales. Desde la integración de Grandes Modelos de Lenguaje (LLMs) hasta arquitecturas multi-agente que colaboran entre sí, creamos software a la medida que transforma datos complejos en flujos de trabajo eficientes y autónomos.</p>
 
             </div>
 
             <div>
-                <img id="assistantAvatar" src="img/seo.png" alt="Asistente de Diseño y Desarrollo Web">
+                <img src="img/seo.png" alt="Asistente de Diseño y Desarrollo Web">
                 <h1>Implementamos</h1>
                 <p>Llevamos la teoría a la práctica integrando la IA directamente en tu día a día. Desplegamos los agentes de IA dentro de tus plataformas y herramientas actuales de forma fluida y sin fricciones. Nos encargamos de la transición técnica, la optimización de procesos y la capacitación de tu equipo, logrando que la automatización inteligente empiece a generar valor, reducir costes y acelerar tus resultados desde el primer momento.</p>
 
